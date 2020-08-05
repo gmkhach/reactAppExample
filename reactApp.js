1de333596aa63f71682f1ad8fd0cba6ad6a921f1
Gabriel Khachatryan <gmkhach@gmail.com>
2020-08-04 23:05:55 -0400

    added an element that updates every second

--- reactApp.js
+++ reactApp.js
@@ -2,7 +2,12 @@ function ReactApp() {
     return React.createElement("div", null, [
         React.createElement("h1", null, "My awesome React app!"),
         React.createElement("p", null, "This exercise concentrates on learning how to create a react app from scratch, using html, javascript, and the react library."),
+        React.createElement("p", null, `Current time: ${new Date().toLocaleString()}.`),
     ])
 }
 
-ReactDOM.render(React.createElement(ReactApp), document.querySelector("#app"))
+// This setInterval function will only rerender the Current time element of the app every 1000 milisecond,
+// because react only rerenders the components that have updated. This is why React is powerful!
+setInterval(function() {
+    ReactDOM.render(React.createElement(ReactApp), document.querySelector("#app"))
+}, 1000);
